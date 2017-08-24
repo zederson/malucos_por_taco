fb2545c738b767f069f5a9713f66941d39c81163
Ederson de Lima <edersondelima@gmail.com>
2017-08-23 23:51:46 -0300

    grafico para iframe

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,19 @@
 class HomeController < ApplicationController
   def index
-    matches = Match::SearchService.by_started(search_param)
-    @matches = HomeDecorate.decorate(matches)
+    search
+  end
+
+  def show
+    search
+    render layout: 'external'
   end
 
   private
+
+  def search
+    matches = Match::SearchService.by_started(search_param)
+    @matches = HomeDecorate.decorate(matches)
+  end
 
   def search_param
     params.dig(:q, :started_at)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get '/admin', to: 'admin#index', as: 'admin'
+  get '/dashboard/general', to: 'home#show', as: 'dashboard_general'
 
   resources :players
   resources :matches
